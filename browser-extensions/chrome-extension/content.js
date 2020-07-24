0c2a9a9da7dc5558c43cab675f79fb9a62b9e1b1
Greg James <gregyjames@users.noreply.github.com>
2020-07-24 14:16:32 -0500

    Add files via upload

---
+++ browser-extensions/chrome-extension/content.js
@@ -0,0 +1,12 @@
+﻿// content.js
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        var xmlhttp = new XMLHttpRequest();
+        var theUrl = "http://localhost:8888/";
+        //Change request type here
+        xmlhttp.open("POST", theUrl);
+        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        //Add variables here
+        xmlhttp.send(JSON.stringify({ "URL": window.location.href.toString() }));
+    }
+);
